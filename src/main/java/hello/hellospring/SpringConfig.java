e13a39c17b16ac343101b7816c179b555158c8ba
unnokid <kimki0609@gmail.com>
2022-01-05 20:54:45 +0900

    Update H2 database and JdbcMemberRepository.java

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 //자바 코드로 직접 설정, 상황에따라 구현클래스를 변경해야되는경우 좋음
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -17,7 +28,9 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository()
+    {
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
